2a934d831671cb87d15e68b9fbf710ac6cc3ded1
David Herrera <biggiegun@gmail.com>
2021-08-24 17:29:48 -0500

    Primer commit - Spring Daily Bits

---
+++ Script/sesion.js
@@ -0,0 +1,27 @@
+// code here
+
+const getData = async (url) => {
+  
+    const resp = await fetch(url);
+    const data = await resp.json();
+    data.forEach((usuario) => {
+      const { nombre, correo} = usuario; // Object Destructuring =)
+      document.querySelector("#nombreUsuario").innerHTML = nombre;
+      document.querySelector("#correoUsuario").innerHTML = correo;
+    });
+  };
+
+  window.addEventListener("DOMContentLoaded", () => {
+    getData("http://localhost:4000/usuario")  
+});
+
+
+let cerrarSesion = document.getElementById("cerrarSesion");
+
+cerrarSesion.addEventListener("click", () => {
+    location.href = "loadPage.html";
+});
+
+
+
+
